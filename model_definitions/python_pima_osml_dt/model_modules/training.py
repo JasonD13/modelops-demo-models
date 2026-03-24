a63250401d24d6b86f21ed69a56958b873720b13
JasonD13 <jasondsouza139@gmail.com>
2026-03-24 12:47:02 +0000

    Added Diabetes Prediction using osml demo model

--- model_definitions/python_pima_osml_dt/model_modules/training.py
+++ model_definitions/python_pima_osml_dt/model_modules/training.py
@@ -31,9 +31,9 @@ def compute_feature_importance(model,X_train):
     for col, val in sorted(zip(X_train.columns, model.tree_.compute_feature_importances()),key=lambda x:x[1],reverse=True):
         feat_dict[col]=val
     feat_df = pd.DataFrame({'Feature':feat_dict.keys(),'Importance':feat_dict.values()})
-    
+
     return feat_df
-    
+
 
 def plot_feature_importance(fi, img_filename):
     feat_importances = fi.sort_values(['Importance'],ascending = False).head(10)
@@ -41,10 +41,10 @@ def plot_feature_importance(fi, img_filename):
     fig = plt.gcf()
     fig.savefig(img_filename, dpi=500)
     plt.clf()
-    
+
 def train(context: ModelContext, **kwargs):
     aoa_create_context()
-    
+
     # Extracting feature names, target name, and entity key from the context
     feature_names = context.dataset_info.feature_names
     target_name = context.dataset_info.target_names[0]
@@ -69,27 +69,27 @@ def train(context: ModelContext, **kwargs):
         object=scaler.output,
         accumulate = [target_name,entity_key]
     )
-    
+
     scaler.output.to_sql(f"scaler_${context.model_version}", if_exists="replace")
     print("Saved scaler")
-    
-         
+
+
     print("Starting training using teradata osml...")
 
     DT_classifier = osml.DecisionTreeClassifier(random_state=int(context.hyperparams["random_state"])
                                                 ,max_leaf_nodes=int(context.hyperparams["max_leaf_nodes"])
                                                 ,max_features=context.hyperparams["max_features"]
                                                 ,max_depth=int(context.hyperparams["max_depth"]))
-    
+
     DT_classifier.fit(X_train, y_train)
     DT_classifier.deploy(model_name="DT_classifier", replace_if_exists=True)
-        
+
     print("Complete osml training...")
-    
+
     # Calculate feature importance and generate plot
     feature_importance = compute_feature_importance(DT_classifier.modelObj,X_train)
     plot_feature_importance(feature_importance, f"{context.artifact_output_path}/feature_importance")
-    
+
     record_training_stats(
         train_df,
         features=feature_names,
@@ -98,5 +98,5 @@ def train(context: ModelContext, **kwargs):
         feature_importance=feature_importance,
         context=context
     )
-    
+
     print("All done!")
